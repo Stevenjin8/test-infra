5a35c0543dd061d323696083154b23b9c8b19758
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-03-03 09:35:18 -0500

    fix typo

--- infra/gcp/common.tf
+++ infra/gcp/common.tf
@@ -15,7 +15,7 @@ locals {
     "johnbhoward96@gmail.com", # howardjohn
     "daniel.hawton@solo.io",   # dhawton
     "keithmattix2@gmail.com",  # keithmattix
-    "stevenjin8gmail.com",     # Stevenjin8 
+    "stevenjin8@gmail.com",    # Stevenjin8 
   ])
 
 }
